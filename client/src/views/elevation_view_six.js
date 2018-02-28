2ee082c1455686e53f64dd618c2ba50c2ae05804
RunsWritesCode <sophieamullins@gmail.com>
2018-02-28 16:32:19 +0000

    css divs matching up

--- client/src/views/elevation_view_six.js
+++ client/src/views/elevation_view_six.js
@@ -1,5 +1,5 @@
 var elevationViewSix = function(totalElevation) {
-  var pTotalElSixTag = document.querySelector('#elevation-six');
+  var pTotalElSixTag = document.querySelector('elevation-six');
   pTotalElSixTag.innerText = `6 week total elevation: ${totalElevation.toFixed(2)} m`;
 }
 
--- client/src/views/total_view_six.js
+++ client/src/views/total_view_six.js
@@ -1,6 +1,6 @@
 var totalViewSix = function(totalDistance) {
   var pTotalSixTag = document.querySelector('#total-distance-six');
-  pTotalSixTag.innerText = `12 week total distance: ${(totalDistance/1000).toFixed(2)} km`;
+  pTotalSixTag.innerText = `6 week total distance: ${(totalDistance/1000).toFixed(2)} km`;
 }
 
 
